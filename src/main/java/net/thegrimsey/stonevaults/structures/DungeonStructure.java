9d193ac16ffe283d33738434ab1f86ed5b7beae5
TheGrimsey <TheGrimsey@users.noreply.github.com>
2021-07-08 17:12:15 +0200

    Deeper dungeons when needed. Stairs, etc.

--- src/main/java/net/thegrimsey/stonevaults/structures/DungeonStructure.java
+++ src/main/java/net/thegrimsey/stonevaults/structures/DungeonStructure.java
@@ -11,6 +11,7 @@ import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.registry.DynamicRegistryManager;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.HeightLimitView;
+import net.minecraft.world.Heightmap;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.feature.DefaultFeatureConfig;
@@ -20,6 +21,7 @@ import net.thegrimsey.stonevaults.Stonevaults;
 
 public class DungeonStructure extends StructureFeature<DefaultFeatureConfig> {
     public static Identifier START_POOL = new Identifier(Stonevaults.MODID, "startpool_dungeon");
+    public static Identifier START_POOL_LONG = new Identifier(Stonevaults.MODID, "startpool_dungeon_long");
 
     public DungeonStructure(Codec<DefaultFeatureConfig> codec) {
         super(codec);
@@ -32,6 +34,7 @@ public class DungeonStructure extends StructureFeature<DefaultFeatureConfig> {
 
     public static class Start extends MarginedStructureStart<DefaultFeatureConfig> {
         private static StructurePoolFeatureConfig structurePoolFeatureConfig = null;
+        private static StructurePoolFeatureConfig longStructurePoolFeatureConfig = null;
 
         public Start(StructureFeature<DefaultFeatureConfig> structureIn, ChunkPos pos, int referenceIn, long seedIn) {
             super(structureIn, pos, referenceIn, seedIn);
@@ -41,14 +44,18 @@ public class DungeonStructure extends StructureFeature<DefaultFeatureConfig> {
         public void init(DynamicRegistryManager registryManager, ChunkGenerator chunkGenerator, StructureManager manager, ChunkPos pos, Biome biome, DefaultFeatureConfig config, HeightLimitView world) {
             // Position, we don't care about Y as we will just be placed on top on the terrain.
             BlockPos blockPos = new BlockPos(pos.x << 4, 0, pos.z << 4);
+            boolean highDungeon = chunkGenerator.getHeightInGround(blockPos.getX(), blockPos.getZ(), Heightmap.Type.WORLD_SURFACE, world) > (chunkGenerator.getSeaLevel() + 16);
 
             // Initialize structurePoolFeatureConfig if it is null. Doing it everytime we spawn creates garbage so we just make one.
             if (structurePoolFeatureConfig == null)
                 structurePoolFeatureConfig = new StructurePoolFeatureConfig(() -> registryManager.get(Registry.STRUCTURE_POOL_KEY).get(START_POOL), 7);
 
+            if (longStructurePoolFeatureConfig == null)
+                longStructurePoolFeatureConfig = new StructurePoolFeatureConfig(() -> registryManager.get(Registry.STRUCTURE_POOL_KEY).get(START_POOL_LONG), 7);
+
             // Spawn structure.
             StructurePoolBasedGenerator.method_30419(registryManager,
-                    structurePoolFeatureConfig,
+                    highDungeon ? longStructurePoolFeatureConfig : structurePoolFeatureConfig,
                     PoolStructurePiece::new, chunkGenerator, manager, blockPos, this, this.random, false, true, world);
 
             this.children.forEach(structurePiece -> {
